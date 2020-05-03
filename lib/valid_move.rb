eba80cc13bbb72ec727454265fbfac008f78e26d
Chaim Bryski <luzybry94@gmail.com>
2020-05-03 04:43:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,10 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if (index - 1).between?
+  if (index - 1).between?(0, 8) && !position_taken?
+    true
+  else
+    false
+  end
   
 end
 
